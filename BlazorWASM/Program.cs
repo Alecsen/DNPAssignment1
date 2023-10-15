dae1b57e11aef68611a2a663e983cf71db7b205f
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-10-15 12:15:10 +0200

    Blazor added and we have a working version

--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorWASM.Data;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+builder.Services.AddScoped(
+    sp => 
+        new HttpClient { 
+            BaseAddress = new Uri("https://localhost:7107/") 
+        }
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

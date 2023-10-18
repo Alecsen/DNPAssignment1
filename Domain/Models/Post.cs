ae3f4e2c0e5f6088991638ad549a6dc39deefb69
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-10-18 17:58:47 +0200

    Adding atytribute

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -5,7 +5,7 @@ public class Post
     public int Id { get; set; }
     public User Owner { get; }
     public string Title { get; }
-    public string body { get; }
+    public string body { get; set; }
 
     public Post(User owner, string title, string body)
     {
